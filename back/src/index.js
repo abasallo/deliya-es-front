402da8548384f6ae67a2b2cf8d87280e718ac2ca
Alvaro Basallo <alvaro@basallo.es>
2020-02-25 15:02:00 +0100

    [Back] - Sequelize with sqlite minimal server

--- back/src/index.js
+++ back/src/index.js
@@ -3,7 +3,21 @@ import { ApolloServer } from 'apollo-server'
 import typeDefs from './schema'
 import resolvers from './resolvers'
 
-require('dotenv').config()
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const Sequelize = require('sequelize')
+const dbDialect = process.env.DB_DIALECT || 'sqlite'
+const dbPath = process.env.DB_PATH || 'deliya.sqlite'
+const sequelize = new Sequelize({
+  dialect: dbDialect,
+  storage: dbPath
+})
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err))
 
 new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true })
   .listen({ port: process.env.PORT || 4000 })
