f35b8852fe8287380795e5ea56accdca971be4a0
Alvaro Basallo <alvaro@basallo.es>
2020-02-25 12:37:26 +0100

    [Back] - Env file for port

--- back/src/index.js
+++ back/src/index.js
@@ -3,6 +3,8 @@ import { ApolloServer } from 'apollo-server'
 import typeDefs from './schema'
 import resolvers from './resolvers'
 
+require('dotenv').config()
+
 new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true })
   .listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => console.log(`Server ready at ${url}`))
