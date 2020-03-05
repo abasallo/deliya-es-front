66fa77a374bd4d090232eaa0d0367801974d96d5
Alvaro Basallo <alvaro@basallo.es>
2020-03-05 21:31:05 +0100

    [Front] - First carousel version

--- front/src/views/dashboard/Dashboard.js
+++ front/src/views/dashboard/Dashboard.js
@@ -6,14 +6,31 @@ import './Dashboard.styles.scss'
 
 import Logo from '../../images/logo.png'
 
+import Img1 from '../../images/1.jpg'
+import Img2 from '../../images/2.jpg'
+import Img3 from '../../images/3.jpg'
+import Img4 from '../../images/4.jpg'
+import Img5 from '../../images/5.jpg'
+
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+
+import Slider from 'react-slick'
 
 import Copyright from '../../components/Copyright'
+
+const settings = {
+  dots: true,
+  fade: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
 
 const Dashboard = props => {
   return (
@@ -22,7 +39,7 @@ const Dashboard = props => {
         <Toolbar>
           <img src={Logo} alt="Logo" />
           <Typography variant="h6" className="Title">
-            Event Dashboard
+            Main
           </Typography>
           <Button>{props.loginState.email}</Button>
           <Button
@@ -35,7 +52,15 @@ const Dashboard = props => {
           </Button>
         </Toolbar>
       </AppBar>
-      <Container className="MainContainer">Main Container</Container>
+      <Container className="MainContainer">
+        <Slider {...settings}>
+          <img src={Img1} className="SliderPictures" alt="" />
+          <img src={Img2} className="SliderPictures" alt="" />
+          <img src={Img3} className="SliderPictures" alt="" />
+          <img src={Img4} className="SliderPictures" alt="" />
+          <img src={Img5} className="SliderPictures" alt="" />
+        </Slider>
+      </Container>
       <Box mt={8}>
         <Copyright />
       </Box>
