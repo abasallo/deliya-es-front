f65bf7ee6264beca9c4b717e253a0c79132273fb
Alvaro Basallo <alvaro@basallo.es>
2020-03-31 10:56:58 +0200

    .env removed from Git

--- src/services/graphql/apolloClient.js
+++ src/services/graphql/apolloClient.js
@@ -2,7 +2,9 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+const REACT_APP_GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL
+
 export const apolloClient = new ApolloClient({
-  link: createHttpLink({ uri: process.env.REACT_APP_GRAPHQL_URL }),
+  link: createHttpLink({ uri: REACT_APP_GRAPHQL_URL }),
   cache: new InMemoryCache()
 })
