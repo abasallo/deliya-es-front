b683dd56771f77ca8d67f010c544ea0986ca1f82
Alvaro Basallo <alvaro@basallo.es>
2020-04-17 08:19:39 +0200

    BACKEND_URL to REACT_APP_BACKEND_URL

--- src/services/graphql/apolloClient.js
+++ src/services/graphql/apolloClient.js
@@ -5,6 +5,6 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 const LOCAL_GRAPHQL_BACKEND_URL = 'http://localhost:4000/graphql'
 
 export const apolloClient = new ApolloClient({
-  link: createHttpLink({ uri: process.env.BACKEND_URL || LOCAL_GRAPHQL_BACKEND_URL }),
+  link: createHttpLink({ uri: process.env.REACT_APP_BACKEND_URL || LOCAL_GRAPHQL_BACKEND_URL }),
   cache: new InMemoryCache()
 })
